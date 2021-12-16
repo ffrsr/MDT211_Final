cf094f79c40620dac6cc8b7e769d7dbd501c5cc2
ffrsr <rossarin.869@mail.kmutt.ac.th>
2021-12-16 12:22:17 +0700

    ข้อ9

---
+++ ข้อ9/ข้อ9/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ข้อ9
+{
+    class Program
+    {
+        static void AddNode(ref Node node)
+        {                       
+                Console.WriteLine("Input skill name :");
+                string Skillname = Console.ReadLine();
+                Console.WriteLine("Add dependency for {0}? (Y/N)", Skillname);
+                char answer = char.Parse(Console.ReadLine());
+
+
+                if (answer == 'Y')
+                {
+                    string message = Console.ReadLine();
+                    node = new QuestionNode(message);
+                    AddNode(ref (node as QuestionNode).Left);
+                    AddNode(ref (node as QuestionNode).Right);
+
+                    Console.WriteLine("Add dependency for {0}? (Y/N)", Skillname);
+                    answer = char.Parse(Console.ReadLine());
+                }
+                else if (answer == 'N')
+                {
+                    Console.WriteLine("Add dependency for {0}? (Y/N)", Skillname);
+                    answer = char.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Input skill name :");
+                    string message = Console.ReadLine();
+                    node = new DeceaseNode(message);
+                }            
+        }
+       static void Main(string[] args)
+        {
+            Node root = null;
+            AddNode(ref root);
+
+            Node ptr = root;
+            while (true)
+            {
+                Console.WriteLine(ptr.Message);
+                if (ptr is QuestionNode)
+                {
+                    char answer = char.Parse(Console.ReadLine());
+                    if (answer == 'Y')
+                    {
+                        ptr = (ptr as QuestionNode).Left;
+                    }
+                    else
+                    {
+                        ptr = (ptr as QuestionNode).Right;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+
+    class Node
+    {
+        public string Message;
+
+        public Node(string messageValue)
+        {
+            Message = messageValue;
+        }
+    }
+
+    class QuestionNode : Node
+    {
+        public Node Left;
+        public Node Right;
+
+        public QuestionNode(string messageValue) : base(messageValue) { }
+    }
+    class DeceaseNode : Node
+    {
+        public DeceaseNode(string messageValue) : base(messageValue) { }
+    }
+}
