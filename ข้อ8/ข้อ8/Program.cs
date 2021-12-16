77fbed3d6ce454937a36f031d51d3b93ffae1546
ffrsr <rossarin.869@mail.kmutt.ac.th>
2021-12-16 12:22:05 +0700

    ข้อ8

---
+++ ข้อ8/ข้อ8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ข้อ8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int InputNumber = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < InputNumber; i++)
+            {
+                string InputName = Console.ReadLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
